ec714174c0bb418658fa529a10e810e7a2809a1d
Bismakhan21 <bismakhan2022@gmail.com>
2024-03-07 13:37:19 +0500

    completed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Wheather } from './Component/Wheather_app/Wheather';
+
+
+
+function App() {
+
+  return (
+    <>
+      
+     <Wheather />   
+    </>
+  )
+}
+
+export default App;
